bad46b6606c7ff02ec50dce52b6b48c85db4242e
andreabr <andrezinhodorio@gmail.com>
2017-07-24 15:59:14 -0300

    Segundo Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,68 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" type="text/css" href="{{asset('lib/materialize/dist/css/materialize.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <title>Laravel</title>
+    
 
-   
-
-   
+    
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+    <nav>
+        <div class="nav-wrapper blue">
+            <div class="container">
+              <a href="#!" class="brand-logo">Logo</a>
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="right hide-on-med-and-down">
+                <li><a href="#">Home</a></li>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="#">Home</a></li>
+                
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    @yield('content')
+@yield('content')
 
-    <script src="{{asset('lib/jquery/dist/jquery.js')}}"></script>
-    <script src="{{asset('lib/materialize/dist/css/materialize.css')}}"></script>
-   
-    
+<script src="{{asset('lib/jquery/dist/jquery.js')}}"></script>
+<script src="{{asset('lib/materialize/dist/js/materialize.js')}}"></script>
+<script src="{{asset('js/init.js')}}"></script>
+
+
 </body>
 </html>
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.site')
+
+@section('content')
+@include('layouts._site._slides')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+               
+                <div class="panel-body">
+                    Home
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
