76b06de6f6197ac467d354e6460b9e389f341609
andreabr <andrezinhodorio@gmail.com>
2017-08-21 16:25:06 -0300

    Quarto Commit

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.site')
+
+@section('content')
+
+<div class="container">
+	<h3>Contato</h3>   
+</div>
+@endsection
---
+++ resources/views/site/sobre.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.site')
+
+@section('content')
+
+<div class="container">
+	<div class="row section">
+		<h3 align="center">Sobre</h3>
+		<div class="divider"></div>
+	</div>
+	<div class="row section">
+		<div class="col s12 m6">
+			<img class="responsive-img" src="{{ asset }}" alt="">
+		</div>
+		<div class="col s12 m6">
+			
+		</div>
+	</div>
+</div>
+@endsection
